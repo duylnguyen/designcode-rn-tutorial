4af8a368b3b9df57647da58f65bedadb62ea53fc
Duy Nguyen <duy_nobita@icloud.com>
2020-03-13 20:20:20 -0400

    Using Redux to manage state actions

--- components/Menu.js
+++ components/Menu.js
@@ -3,6 +3,20 @@ import styled from "styled-components";
 import { Animated, TouchableOpacity, Dimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import MenuItem from "./MenuItem";
+import { connect } from "react-redux";
+
+function mapStateToProps(state) {
+  return { action: state.action };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    closeMenu: () =>
+      dispatch({
+        type: "CLOSE_MENU"
+      })
+  };
+}
 
 const screenHeight = Dimensions.get("window").height;
 
@@ -12,15 +26,25 @@ class Menu extends React.Component {
   };
 
   componentDidMount() {
-    Animated.spring(this.state.top, {
-      toValue: 0
-    }).start();
+    this.handleToggleMenu();
+  }
+
+  componentDidUpdate() {
+    this.handleToggleMenu();
   }
 
   handleToggleMenu = () => {
-    Animated.spring(this.state.top, {
-      toValue: screenHeight
-    }).start();
+    if (this.props.action == "openMenu") {
+      Animated.spring(this.state.top, {
+        toValue: 0
+      }).start();
+    }
+
+    if (this.props.action == "closeMenu") {
+      Animated.spring(this.state.top, {
+        toValue: screenHeight
+      }).start();
+    }
   };
 
   render() {
@@ -32,7 +56,7 @@ class Menu extends React.Component {
           <Subtitle>Designer at Design+Code</Subtitle>
         </Cover>
         <TouchableOpacity
-          onPress={this.handleToggleMenu}
+          onPress={this.props.closeMenu}
           style={{
             position: "absolute",
             top: 120,
@@ -60,7 +84,7 @@ class Menu extends React.Component {
   }
 }
 
-export default Menu;
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
 
 const Image = styled.Image`
   position: absolute;
@@ -126,7 +150,7 @@ const items = [
   },
   {
     icon: "ios-compass",
-    title: "Leanr React",
+    title: "Learn React",
     text: "start course"
   },
   {
